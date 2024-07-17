b1d5b498afde2fea6dd26d7adf6fa0659d1dd0d0
Maxime <maxime.loir@lepoles.org>
2024-07-17 16:52:00 +0200

    17/07/24

--- exercices/exo4.js
+++ exercices/exo4.js
@@ -24,3 +24,27 @@ for (let i = 1; i <= 100; i++) {
   nombresEntiers.push(i);
 }
 console.log(nombresEntiers);
+
+const carresNbEntiers = [];
+for (let i = 2; i <= 100; i += 2) {
+  carresNbEntiers.push([i, i * i]);
+}
+console.log(carresNbEntiers);
+
+/*4. Ecrire la fonction superieur(a, b)
+		- Cette fonction retourne TRUE si a est supérieur à b sinon retourne FALSE
+        Par exemple :
+        superieur(46, 17) retourne TRUE
+        superieur(17, 46) retourne FALSE
+        superieur(17, 17) retourne FALSE
+        
+        En utilisant la fonction superieur(), comparez 2 variables (nb1 et nb2) 
+        	et affichez :
+            "nb1 est supérieur à nb2" ou "nb1 est inférieur à nb2" 
+            (nb1 et nb2 doivent être remplacé par leur valeur)
+    	Utilisez cette fonction avec les différentes valeurs suivantes
+            nb1 = 15; 	nb2 = 7;
+            nb1 = 52; 	nb2 = 324;
+            nb1 = 9; 	nb2 = 10;
+            nb1 = 4; 	nb2 = -52;
+*/
---
+++ exercices/tp.js
@@ -0,0 +1,5 @@
+/* Exercice 1 */
+document.querySelector("#btChange").addEventListener("click", function (evt) {
+  document.querySelector(".carre.bleu").computedStyleMap.backgroundColor =
+    "green";
+});
